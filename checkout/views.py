0ce8d886f739cdee62dc8fe397b8d55f8c140b10
leah-jala <leahlafond@protonmail.com>
2023-02-25 15:26:17 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MfOcbG7hRgxRTd5T3mfmU4hvhOGHMibXtS9q924Yl0eO67i8i5gJPWhvMrsHhkBFhXJoXT11Y5RZnP16m6adYWK00HRADgpAt',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
