8e22475b5657354acd239d78b2c1eb5cddc03184
leah-jala <leahlafond@protonmail.com>
2023-03-13 17:55:08 +0000

    set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 HEROKU_API_KEY = os.environ.get('HEROKU_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['leahs-boutique-ado.herokuapp.com', 'localhost']
 
